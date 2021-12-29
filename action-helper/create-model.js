99413d3d8bb15f29be330a72f44480ddabefd444
wangchaofan <wangchaofan@cmhi.chinamobile.com>
2021-12-29 10:37:27 +0800

    fix: edit create-model, id change to UUID

--- action-helper/create-model.js
+++ action-helper/create-model.js
@@ -3,10 +3,10 @@ import { upperHead } from "../util/function.js"
 export default function createModel (name) {
   const modelName = upperHead(name)
   return `module.exports = app => {
-  const { STRING, INTEGER, BOOLEAN } = app.Sequelize
+  const { STRING, UUIDV4, UUID } = app.Sequelize
 
   const ${modelName} = app.model.define('${name}', {
-    id: { type: INTEGER, primaryKey: true, autoIncrement: true },
+    id: { type: UUID, primaryKey: true, defaultValue: UUIDV4 },
     name: { type: STRING },
   }, {
     underscored: true,
